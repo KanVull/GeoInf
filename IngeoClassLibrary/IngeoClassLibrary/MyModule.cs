857b4eaa93fe605bd7c1081a43bb74407e354cb5
KanVull <minirorus7@gmail.com>
2021-12-08 15:54:39 +0300

    Laba 1

---
+++ IngeoClassLibrary/IngeoClassLibrary/MyModule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Ingeo;
+using System.Windows.Forms;
+
+namespace IngeoClassLibrary
+{
+    // Модуль подключения ГИС
+    [ComVisible(true)]
+    public class MyModule
+    {
+        public IIngeoApplication _fApplication;
+
+        public void Init(IIngeoApplication app)
+        {
+            _fApplication = app;
+        }
+
+        public void Done()
+        {
+            _fApplication = null;
+        }
+        // Метод отображения формы
+        void ShowIngeoPopupForm(IIngeoApplication ingeo)
+        {
+            if (ingeo == null)
+                MessageBox.Show("Модуль не инициализирован!");
+            else
+            {
+                var newForm = new mainForm(ingeo);
+                newForm.Show(new IngeoMainWindowHandle(ingeo));
+            }
+        }
+
+        public void ShowPopupForm()
+        {
+            ShowIngeoPopupForm(_fApplication);
+        }
+    }
+    // Класс обработчика главной формы ГИС
+    public class IngeoMainWindowHandle : IWin32Window
+    {
+        private readonly IntPtr _fIngeoMainWindowHandle;
+
+        public IngeoMainWindowHandle(IIngeoApplication ingeo)
+        {
+            _fIngeoMainWindowHandle = new IntPtr(ingeo.MainWindow.Handle);
+        }
+
+        IntPtr IWin32Window.Handle
+        {
+            get { return _fIngeoMainWindowHandle; }
+        }
+    }
+}
---
+++ IngeoClassLibrary/IngeoClassLibrary/mainForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using Ingeo;
+
+namespace IngeoClassLibrary
+{
+    public partial class mainForm : Form
+    {
+        public IIngeoApplication FApplication;
+        // конструктор главной формы
+        public mainForm(IIngeoApplication fApplication)
+        {
+            FApplication = fApplication;
+            InitializeComponent();
+        }
+    }
+}
+
